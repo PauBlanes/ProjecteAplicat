bfa57ecb109fc826d02606cd2ca41cd7b1d2c8f9
PauBlanes_ENTI <paublanespardo@enti.cat>
2019-05-20 11:49:19 +0200

    Added search bar

--- TravelNet/app/src/main/java/paublanes/travelnet/ProfileActivity.java
+++ TravelNet/app/src/main/java/paublanes/travelnet/ProfileActivity.java
@@ -3,7 +3,13 @@ package paublanes.travelnet;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 
@@ -18,6 +24,10 @@ public class ProfileActivity extends AppCompatActivity
     ProfileRouteListFragment listFrag;
     ProfileMapFragment mapFrag;
 
+    //SEARCH
+    MaterialSearchView searchView;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,7 +39,6 @@ public class ProfileActivity extends AppCompatActivity
 
         //Get view references
         findViewById(R.id.fab_add).setOnClickListener(this);
-        findViewById(R.id.btn_signout).setOnClickListener(this);
 
         //Get fragment references
         listFrag = (ProfileRouteListFragment) getSupportFragmentManager().findFragmentById(R.id.listFrag);
@@ -37,6 +46,11 @@ public class ProfileActivity extends AppCompatActivity
 
         //Start firebase listener
         FirebaseManager.getInstance().initLister(routes, this::updateUI);
+
+        //Searchview
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initSearchView();
     }
 
     //TAP EVENTS
@@ -45,10 +59,6 @@ public class ProfileActivity extends AppCompatActivity
         switch (v.getId()){
             case R.id.fab_add:
                 openAddPopup();
-                break;
-            case R.id.btn_signout:
-                Intent i = new Intent(ProfileActivity.this, BlankActivity.class);
-                FirebaseManager.getInstance().logOut(this, () -> startActivity(i));
                 break;
         }
 
@@ -60,7 +70,8 @@ public class ProfileActivity extends AppCompatActivity
         Intent i = new Intent(ProfileActivity.this, RouteDetailActivity.class);
         i.putExtra(Keys.SELECTED_ROUTE, routes.get(selectedRoute));
         startActivity(i);
-    }
+    } //interface from RouteAdapter
+
     //ADD ROUTE
     void openAddPopup() {
         Intent i = new Intent(ProfileActivity.this, AddRouteActivity.class);
@@ -75,5 +86,66 @@ public class ProfileActivity extends AppCompatActivity
     void updateUI() {
         listFrag.myAdapter.notifyDataSetChanged();
         mapFrag.showRoutePoints();
+    }
+
+    //ACTION BAR
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.action_search:
+                return true;
+            case R.id.action_logout:
+                Intent i = new Intent(ProfileActivity.this, BlankActivity.class);
+                FirebaseManager.getInstance().logOut(this, () -> startActivity(i));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void initSearchView() {
+        searchView = findViewById(R.id.search_view);
+        searchView.setEllipsize(true);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(ProfileActivity.this, query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                searchView.setSuggestions(getResources().getStringArray(R.array.proves)); //si ho poso abans es queda la pantalla blanca
+            }
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if (searchView.isSearchOpen()) {
+            searchView.closeSearch();
+        }
+        else{
+            super.onBackPressed();
+        }
+
     }
 }
